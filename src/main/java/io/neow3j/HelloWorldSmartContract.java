c0fd5bad392cc9c2cce66cc929868f169bff29a3
Michael Bucher <michael@axlabs.com>
2021-03-25 13:59:54 +0100

    Added the safe annotation for read-only method

--- src/main/java/io/neow3j/HelloWorldSmartContract.java
+++ src/main/java/io/neow3j/HelloWorldSmartContract.java
@@ -1,14 +1,16 @@
 package io.neow3j;
 
 import io.neow3j.devpack.annotations.ManifestExtra;
+import io.neow3j.devpack.annotations.Safe;
 
 // A simple smart contract with one method 
 // that returns a string and takes no arguments.
 @ManifestExtra(key = "name", value = "HelloWorldSmartContract")
 public class HelloWorldSmartContract {
-    
+
+    @Safe
     public static String bongoCat() {
         return "neowwwwwwwwww";
     }
-    
+
 }
